ee9328268a262d00e8d6b3d3e73852d45cc96f23
deekshitha-km-19 <chintukm19@gmail.com>
2026-08-29 20:35:49 +0530

    Add introductory Python script for Day 4

---
+++ day4-python basics.py
@@ -0,0 +1,6 @@
+# Deekshitha KM - Katteri, Mandya - Day 4
+name = "Deekshitha"
+place = "Katteri, Pandavapura, Mandya"
+goal = "AIML Engineer in Bangalore"
+print(f"Hello! I am {name} from {place}")
+print(f"My goal: {goal}")
