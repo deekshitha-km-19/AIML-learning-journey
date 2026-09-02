e10f1118377da8c6470f969a630e8957533770ea
deekshitha-km-19 <chintukm19@gmail.com>
2026-09-02 20:19:28 +0530

    Day 8 - Data Visualization with Seaborn

---
+++ day-8_visualization.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("house_price.csv")
+
+# 1. Histogram - Price distribution
+plt.figure(figsize=(6,4))
+sns.histplot(df['Price'], kde=True)
+plt.title("House Price Distribution")
+plt.savefig("price_hist.png")
+plt.show()
+
+plt.figure(figsize=(6,4))
+sns.heatmap(df.corr(numeric_only=True), annot=True, cmap='coolwarm')
+plt.title("Correlation Heatmap")
+plt.savefig("heatmap.png")
+plt.show()
