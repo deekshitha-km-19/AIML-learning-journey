9e229bb1d7716d92f3fe912e14a9f928668ea298
deekshitha-km-19 <chintukm19@gmail.com>
2026-09-14 21:19:20 +0530

    Day 21 - Variables

---
+++ variables.py
@@ -0,0 +1,24 @@
+
+student_name = "Deekshitha KM"  
+age = 19                        
+cgpa = 8.5                      
+is_cse_student = True           
+
+print(student_name)
+print(age)
+print(cgpa)
+print(is_cse_student)
+
+
+house_size = 1200      
+house_price = 45       
+location = "Mysore"
+
+print(f"House in {location} with {house_size} sqft costs {house_price} lakhs")
+
+
+accuracy = 0.5
+print(f"Initial Model Accuracy: {accuracy}")
+
+accuracy = 0.85  # After learning
+print(f"After Training Accuracy: {accuracy}")
