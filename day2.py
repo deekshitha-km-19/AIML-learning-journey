58dee48f859791aa3474774af45c77bb663847ea
deekshitha-km-19 <chintukm19@gmail.com>
2026-09-20 21:44:16 +0530

    Added 3 python practice

---
+++ day2.py
@@ -0,0 +1,23 @@
+name = "deeksha" 
+age = 19
+is_student = False 
+weight = 49.5
+x = None 
+print(type(x))
+
+print(type(is_student))
+
+s = "5000"
+print(int(s)+age)
+
+a = 10
+b = 25
+print(a + b)  
+print(a - b) 
+print(a * b) 
+print(a / b) 
+print(a // b)  
+print(a % b)  
+print(a ** b)
+
+print(a+b-a+b*2**3)
---
+++ python3.py
@@ -0,0 +1,28 @@
+first_name = "DEEKSHA"
+last_name = "KM"
+full_name = first_name + " " + last_name
+print(full_name)
+
+message = "This is a Warning! "
+print(message*10)
+
+print(message.upper())
+print(message.lower())
+print(message.strip())
+print(message.replace("Warning", "Error"))
+
+name = '''Deeksha said "hello"
+        darshan said "hi"
+        '''
+print(len(message)) 
+
+name = "Deeksha"
+print(name[4])
+print(name[2:6])
+print(name[2:])
+print(name[:4])
+print(name[-2])
+print(name[::3])
+
+s = "Deeksha \tis a good girl"
+print(s)
