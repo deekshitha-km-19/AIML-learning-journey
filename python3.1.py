6e60b5127dc68cae2259f0760246a3efc985b0ac
deekshitha-km-19 <chintukm19@gmail.com>
2026-09-20 21:55:12 +0530

    Added python practice

---
+++ python3.1.py
@@ -0,0 +1,23 @@
+name = "Deeksha" 
+age = 19
+is_student = True
+weight = 5.0
+x = None 
+print(type(x))
+
+print(type(is_student))
+
+s = "10000"
+print(int(s)+age)
+
+a = 18
+b = 67
+print(a + b)  
+print(a - b)  
+print(a * b)  
+print(a / b)  
+print(a // b)  
+print(a % b)  
+print(a ** b) 
+
+print(a+b-a+b*2**3)
