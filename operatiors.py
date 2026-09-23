4406a719940796486bb15593d88f64d350fdc182
deekshitha-km-19 <chintukm19@gmail.com>
2026-09-23 21:57:46 +0530

    Added python operators

---
+++ operatiors.py
@@ -0,0 +1,50 @@
+#assinment operators
+x = 5   
+x += 3  
+x -= 2 
+x *= 4  
+x /= 6  
+#comparion operators
+a = 10
+b = 20
+
+print(a == b)  
+print(a != b)  
+print(a > b)  
+print(a < b)  
+print(a >= 10)  
+print(b <= 25)  
+
+#logical operators
+x = 5
+y = 10
+z = 15
+
+# and operator
+print(x > 0 and y > 5) 
+
+# or operator
+print(x > 10 or z > 10)  
+
+# not operator
+print(not(x > 10))  
+#membership operators
+my_number = [1, 2, 3, 4, 5]
+my_name = "deeksha"
+
+print(3 in my_number)  
+print(6 not in my_number)
+print("P" in my_name)  
+print("z" not in my_name)  
+
+#arthmatic operators
+a = 10
+b = 3
+print(a + b)  
+print(a - b)  
+print(a * b)  
+print(a / b)  
+print(a // b) 
+print(a % b)  
+print(a ** b)  
+
