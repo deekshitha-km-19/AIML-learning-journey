a15a585ef5492c888142c53e175b86e7f2ab83b1
deekshitha-km-19 <chintukm19@gmail.com>
2026-09-25 22:35:56 +0530

    Added dict.py

---
+++ dict.py
@@ -0,0 +1,8 @@
+karnataka_food = {
+    "Bengaluru": "Bisi Bele Bath",
+    "Mysuru": "Mysore Pak",
+    "Mangaluru": "Neer Dosa"
+}
+print(karnataka_food["Mysuru"])  
+print(karnataka_food.get("Mangaluru"))  
+print(karnataka_food.get("Shivamogga", "Not Found"))  
